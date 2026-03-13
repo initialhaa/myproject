48061a1a12d66999fdf30b5a1a6a83bd1d80662b
intialhaa <initialhaa@gmail.com>
2026-03-13 14:06:58 +0700

    perbaikan controller transaksi

--- app/Http/Controllers/Admin/TransactionController.php
+++ app/Http/Controllers/Admin/TransactionController.php
@@ -8,17 +8,19 @@ use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
-    public function index (Request $request){
+    public function index(Request $request)
+    {
         $pencarian = Transaction::with('user')->orderBy('tanggal_transaksi', 'desc');
-        if ($request->has('search') && !empty($request->search)){
-            $pencarian->where('nama_customer', 'like', '%' .$request->search . '%');
+        if ($request->has('search') && !empty($request->search)) {
+            $pencarian->where('nama_customer', 'like', '%' . $request->search . '%');
         }
-        $transaction = $pencarian->paginate(10);
+        $transactions = $pencarian->paginate(10);
         return view('admin.transactions.index', compact('transactions'));
     }
 
-    public function show (Transaction $transaction){
+      public function show(Transaction $transaction)
+    {
         $transaction->load('details.product', 'user');
-        return view('admin.transactions.show', compact('transaction'));
+        return view('admin.transactions.show', compact('transactions'));
     }
 }
